e84c9edc30574eb4bb3ecf8a90c97d489e096d73
nour-wan <nourwanly77@gmail.com>
2025-03-15 00:00:55 +0300

    edit discount2

--- handcrafts/serializer.py
+++ handcrafts/serializer.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from rest_framework import serializers
 
 from discount.models import Discount
@@ -16,12 +17,20 @@ class DiscounttoHandcraftSerializer(serializers.ModelSerializer):
 class HandcraftWithDiscountSerializer(serializers.ModelSerializer):
     # discounts = DiscounttoHandcraftSerializer(many=True, source='discounthandcraft_set')
     discounts = serializers.SerializerMethodField()
+    discounted_price = serializers.SerializerMethodField()
     class Meta:
         model = Handcraft
         # 
         # fields = "__all__"
-        fields = ['id', 'handcraft_name', 'handcraft_price', 'handcraft_count', 'handcraft_image', 'discounts']
+        fields = ['id', 'handcraft_name', 'handcraft_price', 'handcraft_count', 'handcraft_image', 'discounts','discounted_price']
     def get_discounts(self, obj):
         discounts = Discount.objects.filter(discounthandcraft__handcraft=obj)
         return DiscounttoHandcraftSerializer(discounts, many=True).data       
-        
+    def get_discounted_price(self, obj):
+        discounts = Discount.objects.filter(discounthandcraft__handcraft=obj)
+        original_price = obj.handcraft_price  # لا حاجة لتحويله إلى Decimal إذا كان بالفعل Decimal
+        if discounts.exists():
+            for discount in discounts:
+                discount_percentage = Decimal(discount.precentage) / Decimal(100)
+                original_price -= original_price * discount_percentage
+        return round(original_price, 2)
