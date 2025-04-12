a98f73415d9f024bb2a6cb56d072ab3129cd473b
nour-wan <nourwanly77@gmail.com>
2025-04-12 16:31:41 +0300

    edit get discount 1

--- discount/serializer.py
+++ discount/serializer.py
@@ -4,8 +4,14 @@ from handcrafts.serializer import HandcraftSerializer
 
 from .models import Discount, DiscountHandcraft
 
+class DiscountHandcraftSerializer(serializers.ModelSerializer):
+    handcraft = HandcraftSerializer(read_only=True)
+    class Meta:
+        model = DiscountHandcraft
+        fields = '__all__'  
+        
 class DiscountSerializer(serializers.ModelSerializer):
-    handcrafts = HandcraftSerializer(many=True, source='discounthandcraft_set',read_only=True)
+    handcrafts = DiscountHandcraftSerializer(many=True, source='discounthandcraft_set', read_only=True)
     class Meta:
         model = Discount
         fields = ['name', 'precentage', 'from_date', 'to_date', 'count', 'handcrafts']
@@ -13,10 +19,7 @@ class DiscountSerializer(serializers.ModelSerializer):
 
 
               
-class DiscountHandcraftSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DiscountHandcraft
-        fields = '__all__'        
+      
     # handcrafts = HandcraftSerializer(many=True, source='discounthandcraft_set')
 
     # class Meta:
