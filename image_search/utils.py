68009014b1ace40638f0e945d24ad32049bb3830
nour-wan <nourwanly77@gmail.com>
2025-06-24 18:10:00 +0300

    add image search featuer13

--- image_search/utils.py
+++ image_search/utils.py
@@ -35,7 +35,8 @@ def get_sentence_transformer_model():
     """يحمل نموذج SentenceTransformer مرة واحدة."""
     global _model
     if _model is None:
-        print("تحميل نموذج CLIP-ViT-B-32...")
+        # print("تحميل نموذج CLIP-ViT-B-32...")
+        print("تحميل نموذج all-MiniLM-L6-v2...")
         # تم تغيير النموذج للعمل على السيرفر
         _model = SentenceTransformer('all-MiniLM-L6-v2')
         print("تم تحميل النموذج.")
