ef507209ab5c64a6f47d0b10be39bd454df42d47
nour-wan <nourwanly77@gmail.com>
2025-06-30 18:56:15 +0300

    test apis6

--- handcrafts/models.py
+++ handcrafts/models.py
@@ -15,7 +15,7 @@ class Handcraft(models.Model):
     # handcraft_cost=models.DecimalField(max_digits=10, decimal_places=2,null= True , blank=True)
 
     def __str__(self):
-        return self.name
+        return self.handcraft_name
 
     @property
     def image_url(self):
