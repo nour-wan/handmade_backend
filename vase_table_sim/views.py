97719408ffbefd619c9019ff7fcabe165a4b11bf
nour-wan <nourwanly77@gmail.com>
2025-07-30 20:29:18 +0300

    add vase on table sim4

--- vase_table_sim/views.py
+++ vase_table_sim/views.py
@@ -8,7 +8,7 @@ from django.conf import settings
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from PIL import Image
-
+import cv2
 # استيراد التوابع من ملف ai_functions.py في نفس الـ App
 from .ai_functions import load_yolo_model, remove_background_from_image, add_vase_to_table
 
